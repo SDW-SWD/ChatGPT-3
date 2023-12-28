e93f435a5c75d899054ee4e2be98860c7537fd65
Sehan Dhaneera Wickramsauriya <wickramsuriyashehan@gmail.com>
2023-12-28 16:43:56 +0530

    navigation-bar mobile response implementation

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React ,{useState} from 'react';
 import './navbar.css';
 import {RiMenu3Line,RiCloseLine} from 'react-icons/ri';
 import logo from '../../assets/logo.svg'
 
 const Navbar = () => {
+
+    const [toggleMenu , setToggleMenu] = useState(false);
+    const Menu =()=>(
+        <>
+            <p><a href="#home">Home</a></p>
+            <p><a href="#wgpt3">What is a GTP3?</a></p>
+            <p><a href="#possibility">Open AI</a></p>
+            <p><a href="#fetures">Case Study</a></p>
+            <p><a href="#block">Library</a></p>
+        </>
+    )
+
   return (
     <div className= "gpt3__navbar">
         <div className="gpt3__navbar-links">
@@ -11,16 +23,24 @@ const Navbar = () => {
                 <img src={logo}alt="logo"/>
             </div>
             <div className="gpt3__navbar-links_container">
-                <p><a href="#home">Home</a></p>
-                <p><a href="#wgpt3">What is a GTP3?</a></p>
-                <p><a href="#possibility">Open AI</a></p>
-                <p><a href="#fetures">Case Study</a></p>
-                <p><a href="#block">Library</a></p>
+              <Menu />
             </div>
         </div>
         <div className="gtp3__navbar-sign">
             <p>SignIn</p>
             <button type="button">SignUp</button>
+        </div>
+        <div className="gpt3__navbar-menu">
+            {toggleMenu
+                ? <RiCloseLine color="#fff" size={27} onClick={()=> setToggleMenu(false)}/>
+                : <RiMenu3Line color="#fff" size={27} onClick={()=> setToggleMenu(true)}/>
+            }
+            {toggleMenu && (
+                <div className="gpt3__navbar-menu_container scale-up-center" >
+                    <div className="gtp3__navbar-menu_container-links"></div>
+                    <Menu/>
+                </div>
+            )}
         </div>
     </div>
   )
